4aa103eb4f50b694ef3bd9b2faf64480806d84ce
JeremyMqy <jeremyqiyang@gmail.com>
2022-06-28 11:00:53 +0200

    test again

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -8,6 +8,14 @@ public class Planet{
 
     public Planet(double xP, double yP, double xV,double yV, double m, String img){
         xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
         
+    }
+    public Planet(Planet p){
+        xxPos = p.xxPos;
     }
 }
